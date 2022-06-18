f89503cbe247a2f1e527d00c38acc1c37c4fcc94
zameerejaz <37634499+zameerejaz@users.noreply.github.com>
2022-06-18 15:17:18 +0100

    updated user fetch camp query

--- index.js
+++ index.js
@@ -16,66 +16,101 @@ app.get('/user', async (req, res) => {
 })
 
 
+app.get('/user/camp/:id', async(req,res) => {
+    const id = parseInt(req.params.id);
+    const getUsersFromCamp = await prisma.users.findMany({
+        where: {
+            campid: id
+          }
+    }).catch(error => {
+        console.log("error fetching users from camp")
+    })
+    res.json(getUsersFromCamp)
+})
+
 app.get('/camp', async (req, res) => {
     const getAllCamps = await prisma.org.findMany()
     res.json(getAllCamps)
 })
 
-app.post('/camp',jsonParser, async (req, res) =>{
+app.post('/camp', jsonParser, async (req, res) => {
     console.log("adding new camp")
-    const { Name} = req.body
+    const { Name } = req.body
     const creationDate = new Date()
     console.log("new date", creationDate)
     const camp = await prisma.org.create({
         data: {
-          Name,
-          creationDate
+            Name,
+            creationDate
         },
-      }).catch(error => {
-          console.log(error)
-      })
+    }).catch(error => {
+        console.log(error)
+    })
     res.json(camp)
 })
 
-app.post("/provision", jsonParser,async (req, res) => {
+app.post("/provision", jsonParser, async (req, res) => {
     console.log("adding new provisions")
-    const { Name} = req.body
+    const { Name } = req.body
     const provision = await prisma.provisions.create({
         data: {
-          Name,
+            Name,
         },
-      }).catch(error => {
-          console.log(error)
-      })
+    }).catch(error => {
+        console.log(error)
+    })
     res.json(provision)
 })
 
-app.get("/provision", jsonParser,async (req, res) => {
+app.get("/provision", jsonParser, async (req, res) => {
     const provision = await prisma.provisions.findMany()
     res.json(provision)
 })
 
-app.post('/user',jsonParser ,async (req, res) => {
+app.get("/notification", jsonParser, async (req, res) => {
+    const notification = await prisma.notifications.findMany()
+    res.json(notification)
+})
+
+
+
+app.post("/notification", jsonParser, async (req, res) => {
+    console.log("in here with notifications")
+    const { provisionId, date } = req.body
+    const dateParts = date.split("/");
+    const notifyDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+    const notification = await prisma.notifications.create({
+        data: {
+            provisionId,
+            notifyDate
+        },
+    }).catch(error => {
+        console.log(error)
+    })
+    res.json(notification)
+})
+
+app.post('/user', jsonParser, async (req, res) => {
     console.log("in here with users")
-    const { firstName, lastName, dob,campid,headOfFamily} = req.body
+    const { firstName, lastName, dob, campid, headOfFamily } = req.body
     console.log("the old date", dob)
     const dateParts = dob.split("/");
     const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
     const DOB = dateObject
     const user = await prisma.users.create({
-      data: {
-        firstName,
-        lastName,
-        DOB,
-        campid,
-        headOfFamily
-      },
+        data: {
+            firstName,
+            lastName,
+            DOB,
+            campid,
+            headOfFamily
+        },
     }).catch(error => {
         console.log(error)
     })
     res.json(user)
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
